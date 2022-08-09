6cd72c1926010c911a12a2585b67e426e4acb6a8
gitwarehouse <110898780+gitwarehouse@users.noreply.github.com>
2022-08-09 22:37:28 +0530

    Update dft.m

--- dft.m
+++ dft.m
@@ -1,194 +1,3 @@
-DFT
-
-clc;
-clear all;
-N = input('Number of DFT points = ');
-xn = input('Enter the sequence xn = '); 
-ln = length(xn); %find the length of the sequence
-xn = [xn zeros(1,N-ln)];
-xk = zeros(1,N); 
-ixk = zeros(1,N); 
-%code block to find the DFT of the sequence
-for k = 0:N-1
-for n = 0:N-1
-xk(k+1) = xk(k+1)+(xn(n+1)*exp((-1i)*2*pi*k*n/N));
-end
-end
-t = 0:N-1;
-subplot(2,2,1);
-stem(t,xn);
-ylabel('Amplitude ---->');
-xlabel('n ---->');
-title('Input Sequence ---->');
-grid on;
-magnitude = abs(xk); 
-disp('DFT Sequence = ');
-disp(magnitude);
-%code block to plot the DFT sequence
-t = 0:N-1;
-subplot(2,2,2);
-stem(t,magnitude);
-ylabel('Amplitude ---->');
-xlabel('K ---->');
-title('DFT Sequence ---->');
-grid on;
-phase = angle(xk); 
-disp('Phase = ');
-disp(phase);
-t = 0:N-1;
-subplot(2,2,3);
-stem(t,phase);
-ylabel('Phase ---->');
-xlabel('K ---->');
-title('Phase Response');
-grid on;
-for n = 0:N-1
-for k = 0:N-1
-ixk(n+1) = ixk(n+1)+(xk(k+1)*exp(1i*2*pi*k*n/N));
-end
-end
-ixk = ixk./N;
-%code block to plot the IDFT sequence
-t = 0:N-1;
-subplot(2,2,4);
-stem(t,ixk);
-disp('IDFT Sequence = ');
-disp(ixk);
-ylabel('Amplitude ---->');
-xlabel('n ---->');
-title('IDFT sequence ---->');
-grid on;
-
-
-
-Overall Save
-
-
-clc;
-clear all;
-close all;
-x = [2 -2 8 -2 -2 -3 -2 1 -1 9 1 3];
-h= [1 2 3];
-N=4
-% Code to plot X(n)
-subplot(2,2,1);
-stem(x,'blue');
-xlabel ('n---->');
-ylabel ('Amplitude ---->');
-title('X(n)');
-%Code to plot H(n)
-subplot(2,2,2);
-stem(h,'black');
-xlabel ('n---->');
-ylabel ('Amplitude ---->');
-title(' H(n)');
-% Code to perform Convolution using Overlap Save Method
-lx=length(x);
-lh=length(h);
-m=lh-1;
-x=[zeros(1,m) x zeros(1,N)];
-h=[h zeros(1,N-lh)];
-L=N-lh+1;
-k=floor((length(h))/L);
-for i=0:k
-y=x(1,i*L+1:i*L+N);
-q=mycirconv1(y,h); %Call the mycirconv1 function.
-p(i+1,:)=q;
-end
-p1=p(:,lh:N)';
-p=p1(:)'
-% Representation of the Convoled Signal
-subplot(2,2,3:4);
-stem(p,'red');
-xlabel ('n---->');
-ylabel ('Amplitude ---->');
-title('Convoled Signal');
-
-
-OVERALL ADD:
-
-
-clc;
-clear all;
-Xn = [2 -2 8 -2 -2 -3 -2 1 -1 9 1 3];
-Hn = [1 2 3];
-L=4;
-% Code to plot X(n)
-subplot (2,2,1);
-stem(Xn);
-xlabel ('n---->');
-ylabel ('Amplitude ---->');
-title(' X(n)');
-%Code to plot H(n)
-subplot (2,2,2);
-stem(Hn,'red');
-xlabel ('n---->');
-ylabel ('Amplitude ---->');
-title(' H(n)');
-% Code to perform Convolution using Overlap Add Method
-NXn=length(Xn);
-M=length(Hn);
-M1=M-1;
-R=rem(NXn,L);
-N=L+M1;
-Xn=[Xn zeros(1,L-R)];
-Hn=[Hn zeros(1,N-M)];
-K=floor(NXn/L);
-y=zeros(K+1,N);
-z=zeros(1,M1);
-for k=0:K
-Xnp=Xn(L*k+1:L*k+L);
-Xnk=[Xnp z];
-y(k+1,:)=mycirconv1(Xnk,Hn); %Call the mycirconv function.
-end
-p=L+M1;
-for i=1:K
-y(i+1,1:M-1)=y(i,p-M1+1:p)+y(i+1,1:M-1);
-end
-z1=y(:,1:L)';
-y=(z1(:))'
-%Code to plot the Convolved Signal
-subplot (2,2,3:4);
-stem(y,'black');
-xlabel ('n---->');
-ylabel ('Amplitude ---->');
-title('Convolved Signal');
-
-
-Sampling:
-
-clc;
-clear;
-t = -10:0.1:10;
-T=4;
-fm=1/T;
-x= cos(2*pi*fm*t);
-subplot(2,2,1);
-plot(t,x,'linewidth',3);
-xlabel('time');
-ylabel('amplitude');
-grid;
-title('input signal');
-n1=(-10:1:10);
-fs=[fm 2*fm 8*fm];
-label1 = {'under sampling','uniform sampling' ,'over sampling' };
-for i=1:length(fs)
-z(i,:)= cos(2*pi*fm*n1/fs(i));
-subplot(2,2,i+1);
-stem(n1,z(i,:),'linewidth' ,3);
-xlabel('number of samples');
-ylabel('amplitude');
-hold on;
-subplot(2,2,i+1);
-plot(n1,z(i,:),'linewidth',3);
-xlabel('time');
-ylabel('amplitude');
-grid;
-title(label1(i));
-end
-
-
-
 Matrix Method:
 
 
@@ -257,6 +66,252 @@ y(n) = y(n) + x(n)*h(n-k+1);
 end
 end
 y
+
+
+
+
+
+
+DFT
+
+clc;
+clear all;
+N = input('Number of DFT points = ');
+xn = input('Enter the sequence xn = '); 
+ln = length(xn); %find the length of the sequence
+xn = [xn zeros(1,N-ln)];
+xk = zeros(1,N); 
+ixk = zeros(1,N); 
+%code block to find the DFT of the sequence
+for k = 0:N-1
+for n = 0:N-1
+xk(k+1) = xk(k+1)+(xn(n+1)*exp((-1i)*2*pi*k*n/N));
+end
+end
+t = 0:N-1;
+subplot(2,2,1);
+stem(t,xn);
+ylabel('Amplitude ---->');
+xlabel('n ---->');
+title('Input Sequence ---->');
+grid on;
+magnitude = abs(xk); 
+disp('DFT Sequence = ');
+disp(magnitude);
+%code block to plot the DFT sequence
+t = 0:N-1;
+subplot(2,2,2);
+stem(t,magnitude);
+ylabel('Amplitude ---->');
+xlabel('K ---->');
+title('DFT Sequence ---->');
+grid on;
+phase = angle(xk); 
+disp('Phase = ');
+disp(phase);
+t = 0:N-1;
+subplot(2,2,3);
+stem(t,phase);
+ylabel('Phase ---->');
+xlabel('K ---->');
+title('Phase Response');
+grid on;
+
+
+IDFT
+
+for n = 0:N-1
+for k = 0:N-1
+ixk(n+1) = ixk(n+1)+(xk(k+1)*exp(1i*2*pi*k*n/N));
+end
+end
+ixk = ixk./N;
+%code block to plot the IDFT sequence
+t = 0:N-1;
+subplot(2,2,4);
+stem(t,ixk);
+disp('IDFT Sequence = ');
+disp(ixk);
+ylabel('Amplitude ---->');
+xlabel('n ---->');
+title('IDFT sequence ---->');
+grid on;
+
+
+
+Overall Save
+
+
+clc;
+clear all;
+close all;
+x = [2 -2 8 -2 -2 -3 -2 1 -1 9 1 3];
+h= [1 2 3];
+N=4
+% Code to plot X(n)
+subplot(2,2,1);
+stem(x,'blue');
+xlabel ('n---->');
+ylabel ('Amplitude ---->');
+title('X(n)');
+%Code to plot H(n)
+subplot(2,2,2);
+stem(h,'black');
+xlabel ('n---->');
+ylabel ('Amplitude ---->');
+title(' H(n)');
+% Code to perform Convolution using Overlap Save Method
+L=7; M=length(h);
+N=L-M+1;
+lx=length(x);
+h=[h zeros(1,(L-M))];
+x=[x zeros(1,(N-rem(lx,N)))];
+x1=[zeros(1,(M-1)) x(1:N)];
+for i=1:length(x)/N-1
+  z(i,:)=x(i*N-1:(i+1)*N);
+  y(i,:)= cconv(z(i,:),h,L);end
+y=[cconv(x1,h,L);y];
+P=y(:,(M:L));
+P=P';
+final=P(:)
+% Representation of the Convoled Signal
+subplot(2,2,3:4);
+stem(final,'red');
+xlabel ('n---->');
+ylabel ('Amplitude ---->');
+title('Convoled Signal');
+
+
+OVERALL ADD:
+
+
+clc
+clear
+x=[2 -2 8 -2 -2 -3 -2 1 -1 9 1 3];
+h=[1 2 3];
+L=7; M=length(h);
+N=L-M+1;
+lx=length(x);
+h=[h zeros(1,(L-M))];
+x=[x zeros(1,(N-rem(lx,N)))];
+for i=1:length(x)/N
+    z(i,:)=[x(((i-1)*N+1):i*N) zeros(1,(M-1))];
+    y(i,:)=cconv(z(i,:),h,L);
+end
+for i=1:length(x)/N-1
+  P(i,:)=[zeros(1,(i-1)*N) y(i,:) zeros(1,length(x)-L-(i-1)*N)];
+end
+P(i+1,:)=[zeros(1,i*N) y(i+1, (1:N))];
+final=sum(P)
+
+
+
+
+Sampling:
+
+clc;
+clear;
+t = -10:0.1:10;
+T=4;
+fm=1/T;
+x= cos(2*pi*fm*t);
+subplot(2,2,1);
+plot(t,x,'linewidth',3);
+xlabel('time');
+ylabel('amplitude');
+grid;
+title('input signal');
+n1=(-10:1:10);
+fs=[fm 2*fm 8*fm];
+label1 = {'under sampling','uniform sampling' ,'over sampling' };
+for i=1:length(fs)
+z(i,:)= cos(2*pi*fm*n1/fs(i));
+subplot(2,2,i+1);
+stem(n1,z(i,:),'linewidth' ,3);
+xlabel('number of samples');
+ylabel('amplitude');
+hold on;
+subplot(2,2,i+1);
+plot(n1,z(i,:),'linewidth',3);
+xlabel('time');
+ylabel('amplitude');
+grid;
+title(label1(i));
+end
+
+DTMF:
+
+clc;
+clear;
+fs=4e3;
+t=0:1/fs:0.5-1/fs;
+ver=[697 770 852 941];
+hor=[1209 1336 1477];
+tones=[];
+for k=1:4
+    for m=1:3
+        tone=sum(sin(2*pi*[ver(k);hor(m)].*t))';
+        tones= [tones;tone;zeros(size(tone))];
+    end
+end
+%soundsc(tones,fs)
+S = timetable(seconds(0:length(tones)-1)'/fs ,tones);
+
+
+
+FIR FILTER:
+
+N=50;
+fs=8000;
+fc1=2000;
+fc2=3000;
+wc1= fc1/(fs/2);
+wc2=fc2/(fs/2);
+h = fir1(N,[wc1,wc2],'stop' ,hamming(N+1));
+freqz(h,1,1000,fs)
+
+
+
+low pass:
+
+N=30;
+fs=8000;
+fc=2000;
+wc= fc/(fs/2);
+h = fir1(N,wc,'low' ,hamming(N+1));
+freqz(h,1,1000,fs)
+
+
+
+
+
+IIR filter:
+
+clc; 
+clear;
+%wp = input("enter in rad/sec:")
+%ws = input("enter rad/sec:")
+%rp = input("enter ripple:")
+%rs = input("enter ripple:")
+fs = 1000;
+fp1 = 200;
+fp2 = 300;
+fs1=100;
+fs2=400;
+wp1 = fp1/(fs/2);
+wp2 = fp2/(fs/2);
+ws1 = fs1/(fs/2);
+ws2 = fs2/(fs/2);
+kp =3;
+ks = 60;
+
+[N,wc] = buttord([wp1 wp2],[ws1 ws2],kp,ks);
+[b,a] = butter(N ,wc , 'bandpass');
+freqz(b,a,1000,fs);
+
+
+
+
 
 
 
@@ -421,4 +476,28 @@ int main(void) {
 }
 
 
+
+IIR filter:
+
+#include<stdio.h>
+#include<math.h>
+#define pi 3.1415926
+int fs,T,t,x[256],y[256];
+int i;
+int main()
+{
+	fs=1;
+	T=1/fs;
+	for(i=0,t=0;i<25;i++,t+=T)
+	{
+		x[i]=(sin(2*pi*117.1875*t)+sin(2*pi*3906.25*t)+sin(2*pi*585.9375*t))*1000;
+	}
+	y[0]=0;
+	for(i=1;i<25;i++)
+	{
+		y[i]=0.15835*y[i-1]+0.4208*(x[i]+x[i-1]);
+		printf("y[%d]=%d\n",i,y[i]);
+	}
+	return 0;
+}
 
